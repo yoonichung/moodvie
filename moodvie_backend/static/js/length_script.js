df58f941c11b30366a3a493d6e23371b67eea177
LiaSG <liashergill@gmail.com>
2020-04-30 18:24:44 +0100

    Testing selectedMood

--- moodvie_backend/static/js/length_script.js
+++ moodvie_backend/static/js/length_script.js
@@ -1,4 +1,16 @@
 window.addEventListener("load",function() { 
     let mood = document.getElementById("selectedMood");
     changeBackgroundBasedOnMood(mood);
- }); 
+ });
+
+ function changeBackgroundBasedOnMood(mood) {
+    if ("selectedMood" === "Inspiring") {
+        document.body.style = hsl(186, 59% 47%);
+    } else if ("selectedMood" === "Uplifting") {
+        document.body.style = hsl(33, 78%, 65%);
+    } else if ("selectedMood" === "Thought Provoking") {
+        document.body.style = hsl(79, 54%, 65%);
+    } else ("selectedMood" === "Love") {
+        document.body.style = hsl(302, 62%, 47%);
+    }
+ };
