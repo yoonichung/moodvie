18277304d956d96d7433d841bc86c415371444a6
youngyoonc <56639642+youngyoonc@users.noreply.github.com>
2020-04-30 21:12:13 +0100

    updated JS to change background colour accordingly

--- moodvie_backend/static/js/length_script.js
+++ moodvie_backend/static/js/length_script.js
@@ -1,16 +1,21 @@
 window.addEventListener("load",function() { 
-    let mood = document.getElementById("selectedMood");
-    changeBackgroundBasedOnMood(mood);
+    console.log('loaded');
+    let mood = document.getElementById("selectedMood").innerHTML;
+        console.log(mood);
+        changeBackgroundBasedOnMood(mood);
  });
 
- function changeBackgroundBasedOnMood(mood) {
-    if ("selectedMood" === "Inspiring") {
-        document.body.style = hsl(186, 59% 47%);
-    } else if ("selectedMood" === "Uplifting") {
-        document.body.style = hsl(33, 78%, 65%);
-    } else if ("selectedMood" === "Thought Provoking") {
-        document.body.style = hsl(79, 54%, 65%);
-    } else ("selectedMood" === "Love") {
-        document.body.style = hsl(302, 62%, 47%);
-    }
- };
+function changeBackgroundBasedOnMood(mood) {
+    if (mood == "inspiring") {
+        document.body.style.backgroundColor = 'rgb(' + [50,177,192].join(',') + ')';
+        // document.body.style= hsl(186, 59%, 47%);
+    } else if (mood == "uplifting") {
+        document.body.style.backgroundColor = 'rgb(' + [235, 173, 90].join(',') + ')';
+        // document.body.style = hsl(33, 78% 65%);
+    } else if (mood == "thought-provoking") {
+        document.body.style.backgroundColor = 'rgb(' + [184, 214, 117].join(',') + ')';
+        // document.body.style = hsl(79, 54% 65%);
+    } else {
+        document.body.style.backgroundColor = 'rgb(' + [193, 46, 188].join(',') + ')';
+        }
+}
